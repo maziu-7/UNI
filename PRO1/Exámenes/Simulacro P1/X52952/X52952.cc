cbafe80f5660664b0e23eb6944c64b3f482a640e
maziu-7 <0gtrkt0@protonmail.com>
2023-10-28 21:36:18 +0200

    .

--- PRO1/Exámenes/Simulacro P1/X52952/X52952.cc
+++ PRO1/Exámenes/Simulacro P1/X52952/X52952.cc
@@ -4,6 +4,17 @@ using namespace std;
 int main() {
     int n,m,side;
     while (cin >> n >> m >> side) {
-        for (int i = 0; i < )
+        int gridHeight = n*(side + 1) + 1;
+        int gridWidth = m*(side + 1) + 1;
+        for (int i = 0; i < gridHeight; i++) {
+            for (int j = 0; j < gridWidth; j++) {
+                if (i%(side + 1) == 0 or j%(side + 1) == 0) {
+                    cout << "#";
+                }
+                else cout << " ";
+            }
+            cout << endl;
+        }
+        cout << endl;
     }
 }
