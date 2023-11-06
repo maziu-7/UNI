ff55f677a0949159d3723eab3e3d8de672c74f31
Your Name <0gtrkt0@protonmail.com>
2023-11-06 13:10:06 +0100

    .

--- PRO2/(S7) Modularitat (1)/X73814/X73814.cc
+++ PRO2/(S7) Modularitat (1)/X73814/X73814.cc
@@ -1,12 +1,17 @@
 #include "Estudiant.hh"
-#include "PRO2Excepcio.hh"
+#include <iostream>
 #include <vector>
 using namespace std;
 
 int main() {
     int n;
     cin >> n;
+    vector<Estudiant> v(n);
     for (int i = 0; i < n; ++i) {
         v[i].llegir();
+    }
+
+    for (int i = 0; i < salida.size(); ++i) {
+        salida[i].escriure();
     }
 }
