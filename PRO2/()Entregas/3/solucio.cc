01d7800a78041640e16d53b896351956b9952a2c
maziu-7 <0gtrkt0@protonmail.com>
2023-11-17 16:56:50 +0100

    .

--- PRO2/()Entregas/3/solucio.cc
+++ PRO2/()Entregas/3/solucio.cc
@@ -14,15 +14,20 @@ void Cjt_estudiants::afegir_estudiant(const Estudiant &est, bool& b) {
     //en cas de que no ho estigui:
     else {
         b = false;
+        //
         for (int i = nest - 1; i >= pos_est; --i) {
             vest[i + 1] = vest[i];
         }
         vest[pos_est] = est;
         ++nest;
 
+        //
         if (pos_est <= imax) ++imax;
+        //
         if (est.te_nota()) {
+            //
             if (imax == -1) imax = pos_est;
+            //
             else if (est.consultar_nota() > vest[imax].consultar_nota()) imax = pos_est;
             else if (est.consultar_nota() == vest[imax].consultar_nota()) {
                 if (est.consultar_DNI() < vest[imax].consultar_DNI()) imax = pos_est;
@@ -36,9 +41,13 @@ void Cjt_estudiants::afegir_estudiant(const Estudiant &est, bool& b) {
 void Cjt_estudiants::eliminar_estudiants_sense_nota() {
     int est_sense_nota = 0;
     for (int i = 0; i < nest; ++i) {
-        //fiquem els estudiants sense nota al final del vector
         if (not vest[i].te_nota()) ++est_sense_nota;
+        //reagrupem tots els estudiants que es troben al vector en dos conjunts,
+        //els que tenen nota es posicionen al principi del vector, i justament
+        //després van tots els que no tenen nota
         else vest[i - est_sense_nota] = vest[i];
+        //si 
+        if (imax == i) imax = i - est_sense_nota;
     }
     //modifiquem el nombre d'estudiants ja que els que no
     //tenen nota no ens interessen
