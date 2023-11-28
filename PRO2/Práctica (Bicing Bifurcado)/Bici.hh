495362d1591f5bdb8b34960c0a7822d3b5877c30
maziu-7 <0gtrkt0@protonmail.com>
2023-11-28 16:00:04 +0100

    .

--- PRO2/Práctica (Bicing Bifurcado)/Bici.hh
+++ PRO2/Práctica (Bicing Bifurcado)/Bici.hh
@@ -1,19 +1,54 @@
+/**
+ * @file Bici.hh
+ * @brief Especificación de la clase Bici
+ */
 #ifndef BICI_HH
 #define BICI_HH
 
+#ifndef NO_DIAGRAM
 #include <iostream>
 #include <list>
 using namespace std;
+#endif
 
+/**
+ * @class Bici 
+ * @brief Contiene los viajes que realiza cada bici
+ * en una lista de pares de strings, con su estación
+ * de origen y su estación de destino.
+ */
 class Bici {
 
 private:
-    list<pair<string,string>> viajes; //
+    list<pair<string,string>> viajes_bici; //viajes de cada bici
 
 public:
-    bool existe_bici(string idb);
-    
+    /**
+     * @brief Constructora por defecto
+     * \pre: cierto
+     * \post: una bici Bici vacía donde se guardan los
+     * viajes que ha hecho una bici
+     */
+    Bici();
 
+    /**
+     * @brief Modificadora que añade un viaje a la bici Bici
+     * \pre: dos strings de identificación de estación, origen y destino
+     * \post: si la estación no existe, si la bici no existe,
+     * si la bici no cabe o si origen y destino son iguales,
+     * se produce un mensaje de error, sino, se añade el nuevo
+     * viaje a viajes_bici
+     */
+    void viaje_nuevo(string origen, string destino);
+
+    /**
+     * @brief Escritora que escribe todos los viajes de una bici
+     * \pre: string de identificador de la bici
+     * \post: si la estación no existe escribe un mensaje
+     * de error, sino, escribe en el canal de salida todos
+     * los viajes hechos por la bici identificada
+     */
+    Bici imprimir_viajes(string idb);
 };
 
 #endif
--- PRO2/Práctica (Bicing Bifurcado)/Estacion.hh
+++ PRO2/Práctica (Bicing Bifurcado)/Estacion.hh
@@ -1,18 +1,84 @@
+/**
+ * @file Estacion.hh
+ * @brief Especificación de la clase Estacion
+ */
+
 #ifndef ESTACION_HH
 #define ESTACION_HH
 
-#include <iostream>
-#include <set>
+#include "Bici.hh"
 
+#ifndef NO_DIAGRAM
+#include <map>
+#include <set>
+#endif
+
+
+/**
+ * @class Estacion 
+ * @brief Contiene las bicis que hay en una estación Estacion,
+ * almacenadas en un set de strings, con sus respectivos
+ * identificadores
+ */
 class Estacion {
 
 private:
-    string ide;
-    int capacidad;
+    map<string,Bici> Bicis; //identificador de la bici junto a sus viajes
+    int plazas_libres; //plazas libres de una estación
 
 public:
-    bool existe_estacion(string ide);
+    /**
+     * @brief Constructora por defecto
+     * \pre: cierto
+     * \post: Una clase Estacion vacía para guardar los
+     * identificadores de las bicis que contiene 
+     */
+    Estacion();
 
+    /**
+     * @brief Modificadora que añade la bici a la estación
+     * Estacion
+     * \pre: string identificador de la bici
+     * \post: si la estación no existe, si la bici ya está
+     * en la estación o si la bici no cabe se produce un
+     * mensaje de error 
+     */
+    void alta_bici(string idb, string ide);
+
+    /**
+     * @brief Modificadora que da de baja la bici especificada
+     * \pre: string identificador de la bici
+     * \post: si la bici no existe se produce un mensaje de error
+     * en el canal de salida, en caso contrario, se elimina la bici
+     * del sistema, modificando también las plazas libres de la estación
+     * en la que se elimina la bici 
+     */
+    void baja_bici(string idb);
+
+    /**
+     * @brief Modificadora que altera la capacidad de la estación
+     * especificada
+     * \pre: string identificador de la estación y un entero
+     * estrictamente positivo
+     * \post: si la estación no existe o si el entero es un número
+     * menor a la cantidad de bicis que hay actualmente en la estación,
+     * se produce un error
+     */
+    void modificar_capacidad(int n);
+
+    /**
+     * @brief Consultora que indica si una estación está llena
+     * \pre: cierto
+     * \post: si la estación no existe, se produce un mensaje de error
+     */
+    bool estacion_llena();
+
+    /**
+     * @brief Consultora que indica qué bicis contiene la estación
+     * \pre: string identificador de la estación
+     * \post: si la estación no existe se produce un mensaje de error
+     */
+    void bicis_estacion(string ide);
 };
 
 #endif
