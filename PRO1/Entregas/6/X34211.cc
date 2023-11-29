fc420dc507523618ccacfabde57a5405df84dea1
maziu-7 <0gtrkt0@protonmail.com>
2023-11-29 18:14:58 +0100

    .

--- PRO1/Entregas/6/X34211.cc
+++ PRO1/Entregas/6/X34211.cc
@@ -6,12 +6,13 @@ int main() {
     int n;
     cin >> n;
     vector<int> v(n);
-    for (int i = 0; i < n; ++i) cin >> v[i];
+    for (int i = 0; i < n; ++i) cin >> v[i]; //leemos el vector
 
     string s;
     while (cin >> s) {
         int sum = 0;
         for (int i = 0; i < s.size(); ++i) {
+            //dependiendo de cada char del string, actualizamos sum
             if (s[i] == '+') sum += v[i];
             else sum -= v[i];
         }
