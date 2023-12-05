08c4af6387952de10ee0bd8b425a8b53c68491e3
maziu7 <0gtrkt0@protonmail.com>
2023-12-05 01:42:01 +0100

    .

--- PRO2/Práctica (Bicing Bifurcado)/Bici.cc
+++ PRO2/Práctica (Bicing Bifurcado)/Bici.cc
@@ -1,1 +1,22 @@
-.
+#include "Bici.hh"
+using namespace std;
+
+Bici::Bici() {
+
+}
+
+void Bici::viaje_nuevo(string origen, string destino) {
+
+}
+
+void Bici::imprimir_viajes() {
+    
+}
+
+string Bici::consultar_estacion() {
+
+}
+
+void Bici::mover_bici(string ide) {
+
+}
