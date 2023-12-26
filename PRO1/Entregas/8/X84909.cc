09c15ae65d5af9e7986086be92251464ebc89656
maziu7 <0gtrkt0@protonmail.com>
2023-12-26 18:59:45 +0100

    .

--- PRO1/Entregas/8/X84909.cc
+++ PRO1/Entregas/8/X84909.cc
@@ -15,7 +15,7 @@ struct Class {
 
 typedef vector<vector<Slot> > TableSlots;
 
-int printDay(string day) {
+int which_day(string day) {
     if (day == "monday") return 0;
     if (day == "tuesday") return 1;
     if (day == "wednesday") return 2;
@@ -26,10 +26,7 @@ int printDay(string day) {
 void printSlotClass(const TableSlots& tableslots, int i, int j) {
     int slotClasses = tableslots[i][j].listsubjects.size();
     int spaceCounter = 9;
-    while (slotClasses/10 > 0) {
-        slotClasses /= 10;
-        --spaceCounter;
-    }
+    while (slotClasses/10 > 0) slotClasses /= 10, --spaceCounter;
     for (int x = 0; x < spaceCounter; ++x) cout << ' ';
     cout << tableslots[i][j].listsubjects.size();
 }
@@ -37,16 +34,10 @@ void printSlotClass(const TableSlots& tableslots, int i, int j) {
 void printSlotTeacher(vector <string> slotTeachers, int& replacementsNeeded) {
     int repeatedTeachers = 0;
     sort(slotTeachers.begin(), slotTeachers.end());
-    for (int x = 1; x < slotTeachers.size(); ++x) {
-        if (slotTeachers[x] == slotTeachers[x - 1]) ++replacementsNeeded;
-        ++repeatedTeachers;
-    }
+    for (int x = 1; x < slotTeachers.size(); ++x) if (slotTeachers[x] == slotTeachers[x - 1]) ++replacementsNeeded, ++repeatedTeachers;
     int uniqueTeachers = slotTeachers.size() - repeatedTeachers;
     int spaceCounter = 9;
-    while (uniqueTeachers / 10 > 0) {
-        uniqueTeachers /= 10;
-        --spaceCounter;
-    }
+    while (uniqueTeachers/10 > 0) uniqueTeachers /= 10, --spaceCounter;
     for (int x = 0; x < spaceCounter; ++x) cout << ' ';
     cout << slotTeachers.size() - repeatedTeachers;
 }
@@ -59,12 +50,13 @@ int main() {
     int minHour = 24;
     int replacementsNeeded = 0;
     while (cin >> input.subject >> input.teacher >> day >> input.start >> input.end) {
-        input.day = printDay(day);
+        if (input.subject == "end") break;
+        input.day = which_day(day);
         classes.push_back(input);
         if (input.start < minHour) minHour = input.start;
         if (input.end > maxHour) maxHour = input.end;
     }
-    TableSlots tableslots(maxHour - minHour, vector<Slot> (5, Slot()));
+    TableSlots tableslots (maxHour - minHour, vector<Slot> (5, Slot () ) );
     for (int i = 0; i < classes.size(); ++i) {
         for (int a = classes[i].start - minHour; a < classes[i].end - minHour; ++a) {
             tableslots[a][classes[i].day].listsubjects.push_back(classes[i].subject);
@@ -76,7 +68,7 @@ int main() {
         int current_hour = minHour + i;
         if (current_hour < 10) cout << ' ';
         cout << current_hour;
-        for (int j = 0; j < 5; ++j) printSlotClass (tableslots, i, j);
+        for (int j = 0; j < 5; ++j) printSlotClass(tableslots, i, j);
         cout << endl;
     }
     cout << "number of teachers per slot:" << endl << " h    monday   tuesday wednesday  thursday    friday" << endl;
