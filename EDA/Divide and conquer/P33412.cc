9a1c7f75f830166e8ab78de1ad9328dbdb66075e
maziu7 <0gtrkt0@protonmail.com>
2024-02-27 12:55:19 +0100

    .

---
+++ EDA/Divide and conquer/P33412.cc
@@ -0,0 +1,36 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+bool resis_dico(double x, const vector<double>& v, int i, int j) {
+    if (i > j) return false;
+    int m = (i+j)/2;
+    if (v[m] == x) return true;
+    else if (v[m] > x) {
+        if (m < j and v[m-1] == x) return true;
+        return resis_dico(x, v, i, m-1);
+    }
+    else {
+        if (m > i and v[m-1] == x) return true;
+        return resis_dico(x, v, m-1, j);
+    }
+}
+
+bool resistant_search(double x, const vector<double>& v) {
+    return resis_dico(x, v, 0, v.size()-1);
+}
+
+int main() {
+    int n;
+    while (cin >> n) {
+        vector<double> V(n);
+        for (int i = 0; i < n; ++i) cin >> V[i];
+        int t;
+        cin >> t;
+        while (t--) {
+            double x;
+            cin >> x;
+            cout << resistant_search(x, V) << endl;
+        }
+    }
+}
--- EDA/Divide and conquer/P84219.cc
+++ EDA/Divide and conquer/P84219.cc
@@ -1,1 +1,32 @@
 #include <iostream>
+#include <vector>
+using namespace std;
+
+int position(double x, const vector<double>& v, int left, int right) {
+	int middle = (right - left)/2 + left;
+	if (left > right) return -1;
+	if (left == right) return (v[left] == x) ? left : -1;
+	
+	if (v[middle] == x) return position(x, v, left, middle);
+	else if (v[middle] > x) return position(x, v, left, middle);
+	else return position(x, v, middle+1, right);
+}
+
+int first_occurrence(double x, const vector<double>& v) {
+	return position(x, v, 0, int(v.size()-1));
+}
+
+int main() {
+    int n;
+    while (cin >> n) {
+        vector<double> V(n);
+        for (int i = 0; i < n; ++i) cin >> V[i];
+        int t;
+        cin >> t;
+        while (t--) {
+            double x;
+            cin >> x;
+            cout << first_occurrence(x, V) << endl;
+        }
+    }
+}
