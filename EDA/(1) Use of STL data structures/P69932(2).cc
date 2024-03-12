b973ac978bdddaeb9fd9a5e46aecc81577e3a490
maziu7 <0gtrkt0@protonmail.com>
2024-03-12 13:09:44 +0100

    .

--- EDA/(1) Use of STL data structures/P69932(2).cc
+++ EDA/(1) Use of STL data structures/P69932(2).cc
@@ -3,31 +3,31 @@
 #include <set>
 using namespace std;
 
+int secuencia(const set<int>& S) {
+    if (S.empty()) return 0;
+
+    int c = 1;
+    auto it = S.begin();
+    auto seg = S.begin();
+    ++seg;
+
+    while (seg != S.end()) {
+        if ((*it)%2 != (*seg)%2) ++c;
+        ++it;
+        ++seg;
+    }
+    return c;
+}
+
 int main() {
     string s;
     while (getline(cin, s)) {
+        istringstream iss(s);
+        
         set<int> S;
         int n;
-        int c = 0;
-        istringstream iss(s);
         while (iss >> n) S.insert(n);
-        bool even = true;
-        auto it = S.begin();
-        while (it != S.end()) {
-            if (it == S.begin()) {
-                ++c;
-                even = (*it%2 == 0);
-            }
-            else if (even and *it%2 != 0) {
-                ++c;
-                even = false;
-            }
-            else if (not even and *it%2 == 0) {
-                ++c;
-                even = true;
-            }
-            ++it;
-        }
-        cout << c << endl;
+        
+        cout << secuencia(S) << endl;
     }
 }
